a5d0a8b7969544f67067f8770eda5463e4a20b3d
Jing <boundino@gmail.com>
2020-03-12 01:19:52 +0100

    add MB filein

--- L1/l1config/PbPb2018/docmsDriver.sh
+++ L1/l1config/PbPb2018/docmsDriver.sh
@@ -2,8 +2,9 @@
 
 globaltag=103X_dataRun2_Prompt_v3
 era=Run2_2018
-# filein='file:/afs/cern.ch/work/w/wangj/public/HIZeroBiasReducedFormat/HIRun2018A-v1/FABCF51E-4182-C341-B159-BF5C73482FAA.root' ; rawlabel="rawDataReducedFormat" ;
-filein='file:/afs/cern.ch/work/w/wangj/public/HIEmptyBX/HIRun2018A-v1/596BE573-BF9D-6548-A994-46113B0151EB.root' ; rawlabel="rawDataRepacker" ;
+filein='file:/afs/cern.ch/work/w/wangj/public/HIZeroBiasReducedFormat/HIRun2018A-v1/FABCF51E-4182-C341-B159-BF5C73482FAA.root' ; rawlabel="rawDataReducedFormat" ;
+# filein='file:/afs/cern.ch/work/w/wangj/public/HIEmptyBX/HIRun2018A-v1/596BE573-BF9D-6548-A994-46113B0151EB.root' ; rawlabel="rawDataRepacker" ;
+# filein='file:/afs/cern.ch/work/w/wangj/public/HIMinimumBias2/HIRun2018A-v1/FC3873C2-BDA3-D747-908D-7C29DC301129.root' ; rawlabel="rawDataRepacker" ;
 
 config=l1Ntuple_data2018
 
@@ -14,16 +15,11 @@ cmsDriver.py l1Ntuple -s RAW2DIGI --no_exec \
     --customise=L1Trigger/Configuration/customiseSettings.L1TSettingsToCaloParams_2018_v1_4 \
     --filein=${filein}
 
-# --customise=L1Trigger/L1TNtuples/customiseL1Ntuple.L1NtupleAODRAWEMU \
-
 echo '
 process.caloStage1Digis.InputLabel = cms.InputTag("'${rawlabel}'")
 process.caloStage2Digis.InputLabel = cms.InputTag("'${rawlabel}'")
 process.gmtStage2Digis.InputLabel = cms.InputTag("'${rawlabel}'")
 process.gtStage2Digis.InputLabel = cms.InputTag("'${rawlabel}'")
-# process.RawToDigi = cms.Sequence(process.L1TRawToDigi+process.siPixelDigis+process.ecalDigis+process.ecalPreshowerDigis+process.hcalDigis+process.muonCSCDigis+process.muonDTDigis+process.muonRPCDigis+process.castorDigis+process.scalersRawToDigi+process.tcdsDigis+process.onlineMetaDataDigis+process.muonGEMDigis+process.ctppsRawToDigi+process.muonRPCNewDigis)
-# process.L1NtupleEMU = cms.Sequence(process.l1EventTree+process.l1CaloTowerEmuTree+process.l1uGTEmuTree)
-# process.SimL1EmulatorCore = cms.Sequence(process.SimL1TCalorimeter+process.SimL1TechnicalTriggers+process.SimL1TGlobal)
 ' >> ${config}.py
 
 echo '
@@ -31,11 +27,14 @@ process.HFAdcana = cms.EDAnalyzer("HFAdcToGeV",
     digiLabel = cms.untracked.InputTag("hcalDigis")
 )
 process.hfadc = cms.Path(process.HFAdcana)
+process.schedule.append(process.hfadc)
 ' >> ${config}.py
 
 edmConfigDump ${config}.py >& ${config}_DUMP.py
 
 sed -i 's/"rawDataCollector"/"'${rawlabel}'"/g' ${config}_DUMP.py
+
+# optional
 
 echo '
 import FWCore.ParameterSet.VarParsing as VarParsing
