a890365e23f7f155a28b9aa14d6a2ddfa53ca1c3
boundino <boundino@gmail.com>
2022-05-12 22:43:28 +0200

    eventmatching: plain output

--- L1/evtmatching/backup/evtmatch.cc
+++ L1/evtmatching/backup/evtmatch.cc
@@ -8,8 +8,6 @@
 #include "L1AnalysisEventDataFormat.h"
 // #include "L1AnalysisL1CaloTowerDataFormat.h"
 
-#include "maketree.h"
-
 int evtmatching(TString infforest, TString infl1, TString outfile, Long64_t nentries = -1)
 {
   TFile* fforest = new TFile(infforest);
@@ -17,14 +15,33 @@ int evtmatching(TString infforest, TString infl1, TString outfile, Long64_t nent
 
   TTree* hiroot = (TTree*)fforest->Get("hiEvtAnalyzer/HiTree");
   TTree* skimroot = (TTree*)fforest->Get("skimanalysis/HltTree");
-  TTree* zdcdigiroot = (TTree*)fforest->Get("zdcanalyzer/zdcdigi");
-  TTree* zdcrechitroot = (TTree*)fforest->Get("zdcanalyzer/zdcrechit");
+  // TTree* zdcdigiroot = (TTree*)fforest->Get("zdcanalyzer/zdcdigi");
+  // TTree* zdcrechitroot = (TTree*)fforest->Get("zdcanalyzer/zdcrechit");
   TTree* l1EvtTree = (TTree*)fl1->Get("l1EventTree/L1EventTree");
   // TTree* l1TowerTree = (TTree*)fl1->Get("l1CaloTowerEmuTree/L1CaloTowerTree");
   TTree* l1ADC = (TTree*)fl1->Get("HFAdcana/adc");
 
-  TFile* outf = new TFile(outfile, "recreate");
-  EvtTowerInfoNTuple* nt = new EvtTowerInfoNTuple(l1ADC, hiroot, skimroot, zdcrechitroot, zdcdigiroot);
+  TFile* outf = new TFile(outfile,"recreate");
+  TDirectory* dhiroot = outf->mkdir("hiEvtAnalyzer","");
+  TDirectory* dskimroot = outf->mkdir("skimanalysis","");
+  // TDirectory* dzdcroot = outf->mkdir("zdcanalyzer","");
+  TDirectory* dl1EvtTree = outf->mkdir("l1EventTree","");
+  // TDirectory* dl1TowerTree = outf->mkdir("l1CaloTowerEmuTree","");
+  TDirectory* dl1ADC = outf->mkdir("HFAdcana","");
+
+  dhiroot->cd();
+  TTree* new_hiroot = hiroot->CloneTree(0);
+  dskimroot->cd();
+  TTree* new_skimroot = skimroot->CloneTree(0);
+  // dzdcroot->cd();
+  // TTree* new_zdcdigiroot = zdcdigiroot->CloneTree(0);
+  // TTree* new_zdcrechitroot = zdcrechitroot->CloneTree(0);
+  dl1EvtTree->cd();
+  TTree* new_l1EvtTree = l1EvtTree->CloneTree(0);
+  // dl1TowerTree->cd();
+  // TTree* new_l1TowerTree = l1TowerTree->CloneTree(0);
+  dl1ADC->cd();
+  TTree* new_l1ADC = l1ADC->CloneTree(0);
 
   hiroot->SetBranchStatus("*", 0);
   hiroot->SetBranchStatus("run", 1);
@@ -36,32 +53,28 @@ int evtmatching(TString infforest, TString infl1, TString outfile, Long64_t nent
   L1Analysis::L1AnalysisEventDataFormat *Event = new L1Analysis::L1AnalysisEventDataFormat();
   l1EvtTree->SetBranchAddress("Event", &Event);
 
-  std::cout<<"---- Check event number"<<std::endl;
-  Long64_t nmaptree = hiroot->GetEntries(), nmatchtree = l1EvtTree->GetEntries();
-  // Long64_t nmaptree = l1EvtTree->GetEntries(), nmatchtree = hiroot->GetEntries();
-  std::cout<<std::left<<std::setw(17)<<"[ hiroot"<<"] "<<hiroot->GetEntries()<<std::endl;
-  std::cout<<std::left<<std::setw(17)<<"[ skimroot"<<"] "<<skimroot->GetEntries()<<std::endl;
-  std::cout<<std::left<<std::setw(17)<<"[ zdcdigiroot"<<"] "<<zdcdigiroot->GetEntries()<<std::endl;
-  std::cout<<std::left<<std::setw(17)<<"[ zdcrechitroot"<<"] "<<zdcrechitroot->GetEntries()<<std::endl;
-  std::cout<<std::left<<std::setw(17)<<"[ l1EvtTree"<<"] "<<l1EvtTree->GetEntries()<<std::endl;
-  std::cout<<std::left<<std::setw(17)<<"[ l1ADC"<<"] "<<l1ADC->GetEntries()<<std::endl;
+  std::cout<<"---- Check evt no. for three trees"<<std::endl;
+  std::cout<<hiroot->GetEntries()<<", "<<l1EvtTree->GetEntries()<<", "<<l1ADC->GetEntries()<<std::endl;
+  Long64_t nhiroot = hiroot->GetEntries();
+  Long64_t nl1EvtTree = l1EvtTree->GetEntries();
+  Long64_t nl1ADC = l1ADC->GetEntries();
 
-  std::cout<<"---- Read HI tree events"<<std::endl;
+  std::cout<<"---- Event reading"<<std::endl;
+  Long64_t entries = (nentries<nl1EvtTree&&nentries>0)?nentries:nl1EvtTree;
   std::vector<UInt_t> hiroot_run;
   std::vector<ULong64_t> hiroot_evt;
   std::vector<UInt_t> hiroot_lumi;
-  for(Long64_t k = 0; k<nmaptree; k++)
+  for(Long64_t k = 0; k<nhiroot; k++)
     {
-      if(k % 10000 == 0) std::cout<<k<<" / "<<nmaptree<<"\r"<<std::flush; 
+      if(k % 10000 == 0) std::cout<<k<<" / "<<nhiroot<<"\r"<<std::flush; 
       hiroot->GetEntry(k); //
       hiroot_run.push_back(run);
       hiroot_evt.push_back(evt);
-      hiroot_lumi.push_back(lumi);
+      hiroot_lumi.push_back(lumi);      
     }
-  std::cout<<std::endl<<"---- Matching events"<<std::endl;
+  std::cout<<std::endl<<"---- Extracted event info"<<std::endl;
   int countmatch = 0, lastmatch = -1;
   std::vector<Long64_t> matchingtable;
-  Long64_t entries = (nentries<nmatchtree&&nentries>0)?nentries:nmatchtree;
   for(Long64_t j = 0; j<entries; j++)
     {
       if(j % 1000 == 0) std::cout<<j<<" / "<<entries<<"\r"<<std::flush;
@@ -75,31 +88,23 @@ int evtmatching(TString infforest, TString infl1, TString outfile, Long64_t nent
             break;
           k++;
         }
-      bool matched = no<hiroot_run.size();
-      if(!matched) k = -1;
+      if(no>=nhiroot) k = -1;
       lastmatch = k;
       //
       matchingtable.push_back(k);
       if(j % 10000 == 0)
         {
-          std::cout<<"check matching... ";
-          std::cout<<std::left<<std::setw(8)<<Event->run<<std::setw(5)<<Event->lumi<<std::setw(10)<<Event->event<<" | ";
-          if(matched)
-            std::cout<<std::left<<std::setw(8)<<hiroot_run[k]<<std::setw(5)<<hiroot_lumi[k]<<std::setw(10)<<hiroot_evt[k];
+          std::cout<<Event->run<<"  "<<Event->lumi<<"  "<<Event->event<<" | ";
+          if(k>=0)
+            std::cout<<hiroot_run[k]<<"  "<<hiroot_lumi[k]<<"  "<<hiroot_evt[k];
           std::cout<<std::endl;
         }
-      if(matched)
-        { 
-          countmatch++; 
-          hiroot_run.erase(hiroot_run.begin() + k);
-          hiroot_lumi.erase(hiroot_lumi.begin() + k);
-          hiroot_evt.erase(hiroot_evt.begin() + k);
-        }
+      if(k>=0) { countmatch++; }
     }
   std::cout<<std::endl;
   std::cout<<"countmatch = "<<countmatch<<std::endl;
 
-  std::cout<<"---- Write to tree"<<std::endl;
+  std::cout<<"---- Writing to tree"<<std::endl;
   hiroot->SetBranchStatus("*", 1);
   for(Long64_t j = 0; j<entries; j++)
     {
@@ -108,26 +113,22 @@ int evtmatching(TString infforest, TString infl1, TString outfile, Long64_t nent
       if(k<0) continue;
       hiroot->GetEntry(k); //
       skimroot->GetEntry(k); //
-      zdcdigiroot->GetEntry(k); //
-      zdcrechitroot->GetEntry(k); //
+      // zdcdigiroot->GetEntry(k); //
+      // zdcrechitroot->GetEntry(k); //
       l1ADC->GetEntry(j); //
-      // l1EvtTree->GetEntry(j); //
+      l1EvtTree->GetEntry(j); //
       // l1TowerTree->GetEntry(j); //
 
-      nt->calculate();
-
-      nt->t->Fill();
-
-      // dl1EvtTree->cd();
-      // new_l1EvtTree->Fill();
+      dl1EvtTree->cd();
+      new_l1EvtTree->Fill();
       // dl1TowerTree->cd();
       // new_l1TowerTree->Fill();
-      // dl1ADC->cd();
-      // new_l1ADC->Fill();
-      // dhiroot->cd();
-      // new_hiroot->Fill();
-      // dskimroot->cd();
-      // new_skimroot->Fill();
+      dl1ADC->cd();
+      new_l1ADC->Fill();
+      dhiroot->cd();
+      new_hiroot->Fill();
+      dskimroot->cd();
+      new_skimroot->Fill();
       // dzdcroot->cd();
       // new_zdcdigiroot->Fill();
       // new_zdcrechitroot->Fill();
