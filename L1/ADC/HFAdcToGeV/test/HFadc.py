3f23af3d24b9cea59d6c40201c89b6e2812e582f
boundino <boundino@gmail.com>
2025-07-07 02:23:40 +0200

    up HFadc minimalist config

--- L1/ADC/HFAdcToGeV/test/HFadc.py
+++ L1/ADC/HFAdcToGeV/test/HFadc.py
@@ -20,7 +20,7 @@ process.maxEvents = cms.untracked.PSet(
 )
 
 process.source = cms.Source("NewEventStreamFileReader",
-    fileNames = cms.untracked.vstring('file:/eos/cms/store/t0streamer/Data/PhysicsIonPhysics0/000/393/767/run393767_ls0021_streamPhysicsIonPhysics0_StorageManager.dat')
+    fileNames = cms.untracked.vstring('file:/eos/cms/store/t0streamer/Data/PhysicsIonPhysics0/000/394/153/run394153_ls0090_streamPhysicsIonPhysics0_StorageManager.dat')
 )
 
 process.TFileService = cms.Service("TFileService",
@@ -33,10 +33,9 @@ process.raw2digi_step = cms.Path(process.RawToDigi)
 process.endjob_step = cms.EndPath(process.endOfProcess)
 
 process.HFAdcana = cms.EDAnalyzer("HFAdcToGeV",
-    # digilabel = cms.untracked.InputTag("simHcalUnsuppressedDigis","HFQIE10DigiCollection"), # MC starting from 2025
-    digiLabel = cms.untracked.InputTag("hcalDigis"), # Data and MC before 2025
-    minimized = cms.untracked.bool(False),
-    fillhf = cms.bool(False) # only turn this on when you know how to produce "towerMaker"
+    digiLabel = cms.untracked.InputTag("hcalDigis"),
+    minimized = cms.untracked.bool(True),
+    fillhf = cms.bool(False)
 )
 process.hfadc = cms.Path(process.HFAdcana)
 
@@ -45,38 +44,32 @@ process.schedule = cms.Schedule(process.raw2digi_step,process.hfadc,process.endj
 from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
 associatePatAlgosToolsTask(process)
 
-# process.load("FWCore.MessageLogger.MessageLogger_cfi")
-# process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 
-# process.Trigger = cms.EDFilter( "TriggerResultsFilter",
-#       triggerConditions = cms.vstring(
-#         "HLT_HIZeroBias_v8"
-#          ),
-#       hltResults = cms.InputTag( "TriggerResults", "", "HLT" ),
-#       l1tResults = cms.InputTag( "gtStage2Digis" ),
-#       l1tIgnoreMask = cms.bool( False ),
-#       l1techIgnorePrescales = cms.bool( True ),
-#       daqPartitions = cms.uint32( 1 ),
-#       throw = cms.bool( True )
-# )
-# for path in process.paths:
-#     getattr(process,path)._seq = process.Trigger * getattr(process,path)._seq
+process.Trigger = cms.EDFilter( "TriggerResultsFilter",
+      triggerConditions = cms.vstring(
+        "HLT_OxyZeroBias_v1"
+         ),
+      hltResults = cms.InputTag( "TriggerResults", "", "HLT" ),
+      l1tResults = cms.InputTag( "gtStage2Digis" ),
+      l1tIgnoreMask = cms.bool( False ),
+      l1techIgnorePrescales = cms.bool( True ),
+      daqPartitions = cms.uint32( 1 ),
+      throw = cms.bool( True )
+)
+for path in process.paths:
+    getattr(process,path)._seq = process.Trigger * getattr(process,path)._seq
     
-# from Configuration.Applications.ConfigBuilder import MassReplaceInputTag
-# MassReplaceInputTag(process, old="rawDataCollector", new="rawDataMapperByLabel")
-# MassReplaceInputTag(process, old="rawDataCollector", new="rawPrimeDataRepacker")
-# MassReplaceInputTag(process, old="rawDataCollector", new="rawDataRepacker")
-# delattr(process, "rawPrimeDataRepacker")
-
-import FWCore.ParameterSet.VarParsing as VarParsing
-ivars = VarParsing.VarParsing('analysis')
-ivars.register('streamer',
-                '',
-                VarParsing.VarParsing.multiplicity.singleton,
-                VarParsing.VarParsing.varType.string,
-                "txt")
-ivars.streamer = "/afs/cern.ch/work/w/wangj/RECO2023/CMSSW_13_2_4/src/reco2023/list/PhysicsHIPhysicsRawPrime0_374322.txt"
-ivars.outputFile = "/eos/cms/store/group/phys_heavyions/wangj/L1PbPb2023/adc/HFadc_r374322_HLT_HIZeroBias_v8.root"
-ivars.parseArguments() # get and parse the command line arguments
-process.source.fileNames = open(ivars.streamer, "r").read().splitlines()
-process.TFileService.fileName = ivars.outputFile
+# import FWCore.ParameterSet.VarParsing as VarParsing
+# ivars = VarParsing.VarParsing('analysis')
+# PD = "PhysicsIonPhysics0"
+# ivars.register('streamer',
+#                 '',
+#                 VarParsing.VarParsing.multiplicity.singleton,
+#                 VarParsing.VarParsing.varType.string,
+#                 "txt")
+# ivars.streamer = "/afs/cern.ch/work/w/wangj/RECO2025/CMSSW_15_0_9/src/recoforrun/run2025/list/" + PD + "_393844.txt"
+# ivars.outputFile = "adc_" + PD + "_393844_LS151-170_ZB.root"
+# ivars.parseArguments() # get and parse the command line arguments
+# process.source.fileNames = open(ivars.streamer, "r").read().splitlines()
+# process.TFileService.fileName = ivars.outputFile
