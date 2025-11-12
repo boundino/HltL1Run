4d8d642cdee0d9f61ba8919cd0a189dca74f435a
boundino <boundino@gmail.com>
2025-11-12 18:58:35 +0100

    update hfADC for 25 PbPb raw prime

--- L1/ADC/HFAdcToGeV/readme.md
+++ L1/ADC/HFAdcToGeV/readme.md
@@ -1,10 +1,10 @@
 
-- The collection to be used (HCAL digis) is usually in RAW data if no special `keep` was used.
+- The collection to be used (HCAL digis) is by default only in RAW data if no special `keep` was used.
 <pre>
 process.HFAdcana = cms.EDAnalyzer("HFAdcToGeV",
-    digiLabel = cms.untracked.InputTag("simHcalUnsuppressedDigis","HFQIE10DigiCollection"), # was "hcalDigis" before 2025
+    digiLabel = cms.untracked.InputTag("hcalDigis"), # use ("simHcalUnsuppressedDigis", "HFQIE10DigiCollection") for MC since 2025
     minimized = cms.untracked.bool(True),
-    fillhf = cms.bool(False) # only turn this on when you have or know how to produce "towerMaker"
+    fillhf = cms.bool(False) # only turn this on if there is or you know how to produce "towerMaker"
 )
 process.hfadc = cms.Path(process.HFAdcana)
 </pre>
--- L1/ADC/HFAdcToGeV/test/hfADC.py
+++ L1/ADC/HFAdcToGeV/test/hfADC.py
@@ -1,8 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
-from Configuration.Eras.Era_Run3_2025_OXY_cff import Run3_2025_OXY
+from Configuration.Eras.Era_Run3_pp_on_PbPb_approxSiStripClusters_2025_cff import Run3_pp_on_PbPb_approxSiStripClusters_2025
 
-process = cms.Process('HFADC', Run3_2025_OXY)
+process = cms.Process('HFADC', Run3_pp_on_PbPb_approxSiStripClusters_2025)
 
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
@@ -10,7 +10,7 @@ process.load('FWCore.MessageService.MessageLogger_cfi')
 process.load('Configuration.EventContent.EventContent_cff')
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
 process.load('Configuration.StandardSequences.MagneticField_cff')
-process.load('Configuration.StandardSequences.RawToDigi_Data_cff')
+process.load('Configuration.StandardSequences.RawToDigi_DataMapper_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
@@ -20,14 +20,16 @@ process.maxEvents = cms.untracked.PSet(
 )
 
 process.source = cms.Source("NewEventStreamFileReader",
-    fileNames = cms.untracked.vstring('file:/eos/cms/store/t0streamer/Data/PhysicsIonPhysics0/000/394/153/run394153_ls0090_streamPhysicsIonPhysics0_StorageManager.dat')
+    fileNames = cms.untracked.vstring(
+        'file:/eos/cms/store/t0streamer/Data/PhysicsHIPhysicsRawPrime0/000/399/002/run399002_ls0289_streamPhysicsHIPhysicsRawPrime0_StorageManager.dat'
+    )
 )
 
 process.TFileService = cms.Service("TFileService",
    fileName = cms.string("HFadc.root"))
 
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, '150X_dataRun3_Prompt_v3', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, '151X_dataRun3_Express_v1', '')
 
 process.raw2digi_step = cms.Path(process.RawToDigi)
 process.endjob_step = cms.EndPath(process.endOfProcess)
@@ -44,11 +46,11 @@ process.schedule = cms.Schedule(process.raw2digi_step,process.hfadc,process.endj
 from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
 associatePatAlgosToolsTask(process)
 
-process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
 process.Trigger = cms.EDFilter("TriggerResultsFilter",
       triggerConditions = cms.vstring(
-        "HLT_OxyZeroBias_v1"
+        "HLT_HIZeroBias_HighRate_v8"
       ),
       hltResults = cms.InputTag("TriggerResults", "", "HLT"),
       l1tResults = cms.InputTag("gtStage2Digis"),
@@ -59,6 +61,9 @@ process.Trigger = cms.EDFilter("TriggerResultsFilter",
 )
 for path in process.paths:
     getattr(process,path)._seq = process.Trigger * getattr(process,path)._seq
+
+from Configuration.Applications.ConfigBuilder import MassReplaceInputTag
+MassReplaceInputTag(process, new="rawDataMapperByLabel", old="rawDataCollector")
     
 # import FWCore.ParameterSet.VarParsing as VarParsing
 # ivars = VarParsing.VarParsing('analysis')
@@ -70,6 +75,8 @@ for path in process.paths:
 #                 "txt")
 # ivars.streamer = "/afs/cern.ch/work/w/wangj/RECO2025/CMSSW_15_0_9/src/recoforrun/run2025/list/" + PD + "_393844.txt"
 # ivars.outputFile = "adc_" + PD + "_393844_LS151-170_ZB.root"
+# # ivars.inputFiles = "file:/eos/cms/store/t0streamer/Data/PhysicsHIPhysicsRawPrime0/000/399/002/run399002_ls0289_streamPhysicsHIPhysicsRawPrime0_StorageManager.dat"
 # ivars.parseArguments() # get and parse the command line arguments
 # process.source.fileNames = open(ivars.streamer, "r").read().splitlines()
+# # process.source.fileNames = ivars.inputFiles
 # process.TFileService.fileName = ivars.outputFile
