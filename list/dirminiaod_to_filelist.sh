71f5c2e9c835ee865771eba7e33d05884f5b5b67
boundino <boundino@gmail.com>
2025-11-17 18:22:16 +0100

    add script to generate miniaod filelist

---
+++ list/dirminiaod_to_filelist.sh
@@ -0,0 +1,13 @@
+filepath="${1:-/eos/cms/store/group/phys_heavyions/wangj/RECO2023/miniaod_PhysicsHIForward0_374596}"
+outputfile=${filepath/'*'/''}
+outputfile=${outputfile##*/}.txt
+
+echo $outputfile
+rm $outputfile 2> /dev/null
+
+for i in `ls -d $filepath/*`
+do
+    infile=${i/'/eos/cms'/}
+    echo $infile >> $outputfile
+done
+
